dc8975984879d9b05d1bbff0383a2efb7e2102dc
Ina <inahpakkr@gmail.com>
2022-06-13 15:06:52 +0900

    22

---
+++ Ex02ButtonEvent/MainComponant.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+import{View, Text, Button}  from 'react-native'
+
+class MainComponent extends Component{
+    render(){
+        return (
+            <View>
+                <Text>Hello </Text>
+                <Button title='Button'></Button>
+            </View>
+        )
+    }
+
+
+}
+
+
+export default MainComponent
---
+++ Ex02ButtonEvent/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import MainComponent from './MainComponant.js';
+
+
+AppRegistry.registerComponent(appName, () => MainComponent);
